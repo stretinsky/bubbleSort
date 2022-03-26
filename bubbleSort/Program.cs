a2ae35f8fd74e3418df1038f17d658c0e5df0dab
stretinsky <90929574+stretinsky@users.noreply.github.com>
2022-03-26 10:29:01 +0300

    Add files via upload

---
+++ bubbleSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] arr = new int[n];
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = r.Next(100);
+            }
+
+            Console.Write($"Изначальный массив: ");
+            foreach (var a in arr) { Console.Write($"{a} "); }
+            Console.WriteLine();
+
+            Console.Write($"Отортированный массив: ");
+            foreach (var a in BubbleSort(arr)) { Console.Write($"{a} "); }
+            Console.WriteLine();
+        }
+        static int[] BubbleSort(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
